f8ca14bffb919e05e4b26964df52980dc33583e0
Gosch <dev@gosch.io>
2020-05-15 00:43:15 -0300

    Implements a clearInterval when count ends

--- src/components/Clock.vue
+++ src/components/Clock.vue
@@ -12,18 +12,21 @@ export default {
       minutes: 0,
       seconds: 0,
       elapsed: 0,
+      interval: null,
     };
   },
   mounted() {
     const urlParams = new URLSearchParams(location.search);
     const urlMinutes = urlParams.get("minutes");
     this.minutes = Number(urlMinutes) || 10;
-    setInterval(this.countDown, 1000);
+    this.interval = setInterval(this.countDown, 1000);
   },
   methods: {
     countDown: function() {
       // Se minutos e segundos chegaram ao zero, retorna.
-      if (this.minutes == 0 && this.seconds == 0) return;
+      if (this.minutes == 0 && this.seconds == 0) {
+        return clearInterval(this.interval);
+      }
       // Controla os segundos
       if (this.seconds == 0) {
         this.seconds = 59;
